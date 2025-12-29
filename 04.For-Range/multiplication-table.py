32f651b0644046d6b25380c9f6908883e8246969
Carol Reis <reicaroline@gmail.com>
2025-12-29 18:35:15 -0300

    Renamed exercise and finished it.

---
+++ 04.For-Range/multiplication-table.py
@@ -0,0 +1,7 @@
+"""
+A simple script to print the multiplication table of 5 using a 'for' loop and 'range()'.
+"""
+
+tabuada = 5
+for i in range(0, 11):
+    print(f'{tabuada} * {i} = {tabuada * i}')
